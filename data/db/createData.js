573e2017a25af061e09fa02bba748c4f130ed314
Owain Harris <owainh@gmail.com>
2017-04-19 20:54:56 -0400

    Added SQL queries which write to JSON and CSV. Will be used for upcoming D3.js use

--- data/db/createData.js
+++ data/db/createData.js
@@ -6,7 +6,8 @@
 
 const mysql = require("mysql");
 var fs = require('fs');
-var file1 = './data/json/jobs.json';
+var csv = require("fast-csv");
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -15,20 +16,30 @@ const connection = mysql.createConnection({
     database: 'tl'
 });
 
-module.exports = function createData() {
+//module.exports = function createData() {
 
-    connection.connect();
+connection.connect();
 
-    connection.query('SELECT fk_trafficEmployeeID, firstName, lastName, costPerHour, hoursBillable, sum(minutes) as total_hours FROM entries JOIN employees on fk_trafficEmployeeID = employees.pk_userID GROUP BY fk_trafficEmployeeID, firstName, lastName', function(error, results, fields) {
-        if (!error) {
-            fs.writeFileSync(file1, JSON.stringify(results));
-        } else {
-            throw error;
-        }
-    });
 
-    console.log("Queries saved to file" + '\n');
 
-    connection.end();
+connection.query('SELECT fk_trafficEmployeeID, firstName, lastName, costPerHour, hoursBillable, sum(minutes) as total_hours FROM entries JOIN employees on fk_trafficEmployeeID = employees.pk_userID GROUP BY fk_trafficEmployeeID, firstName, lastName', function(error, results, fields) {
+    if (!error) {
+        fs.writeFileSync('../json/employees.json', JSON.stringify(results));
+        csv.writeToPath("../csv/employees.csv", results, { headers: true })
+            .on("finish", function() {
+                console.log("done!");
+            });
+    } else {
+        throw error;
+    }
+});
 
-};
+
+
+
+
+console.log("Queries saved to file" + '\n');
+
+connection.end();
+
+//};
